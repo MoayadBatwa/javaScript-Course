3401100e9acc193a5c23ded5bc45fa2a7074fe74
MoayadBatwa <moayadbatwa@gmail.com>
2026-07-21 05:49:15 +0300

    promise

--- promise-and-async-await/index.js
+++ promise-and-async-await/index.js
@@ -1,34 +1,60 @@
 
+function walkDog() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
 
+            const walkDog = true;
 
-function walkDog(callback) {
-    setTimeout(() => {
-        console.log('You Walk the Dog 🐕');
-        callback();
-
-    }, 2500);
-}
-
-function cleanKitchen(callback) {
-    setTimeout(() => {
-        console.log('You Clean the Kitchen 🧹');
-        callback();
-
-    }, 2000);
-}
-
-function takeOutTrash(callback) {
-    setTimeout(() => {
-        console.log('You Takeout the Trash ♻️');
-        callback();
-
-    }, 500);
-}
-
-walkDog(() => {
-    cleanKitchen(() => {
-        takeOutTrash(() => {
-            console.log('You Completed All Chores');
-        })
+            if (walkDog) {
+                resolve('You Walk the Dog 🐕');
+            } else {
+                reject("You DID'T Walk the Dog!");
+            }
+            
+        }, 2500);
     })
-});
+}
+
+function cleanKitchen() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+
+            const cleanKitchen = true;
+
+            if (cleanKitchen) {
+                resolve('You Clean the Kitchen 🧹');
+            } else {
+                reject("You DID'T Clean the Kitchen!");
+            }
+
+        }, 2000);
+    })
+}
+
+function takeOutTrash() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+
+            const takeTrash = false;
+
+            if (takeTrash) {
+                resolve('You Take Out the Trash ♻️');
+            } else {
+                reject("You DID'T Take Out the Trash!");
+            }
+
+        }, 500);
+    })
+}
+
+walkDog().then(dog => {
+    console.log(dog);
+    return cleanKitchen();
+}).then(clean => {
+    console.log(clean);
+    return takeOutTrash();
+}).then(trash => {
+    console.log(trash);
+    console.log("All Chores Are Completed");
+}).catch(error => console.error(error));
+
